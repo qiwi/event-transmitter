c470196ce677351a470b987b10dedd25abb9c967
Anton Golub <mailbox@antongolub.ru>
2020-03-05 13:50:23 +0300

    feat: expose flp pipeline from index
    closes #5

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,5 +1,6 @@
 export { createHttpPipe } from './pipes/http'
 export { createMaskerPipe, panMaskerPipe } from './pipes/masker'
+export { createFlpPipeline, eventifyPipe } from './pipes/flp'
 export { createTransmitter, createTransmittable } from './transmitter'
 
 export * from './interfaces'
--- src/test/ts/index.ts
+++ src/test/ts/index.ts
@@ -1,11 +1,17 @@
 import {
   createTransmitter,
-  createHttpPipe
+  createHttpPipe,
+  createFlpPipeline,
+  createTransmittable,
+  createMaskerPipe,
 } from '../../main/ts'
 
 describe('index', () => {
   it('has proper export', () => {
     expect(createTransmitter).toEqual(expect.any(Function))
     expect(createHttpPipe).toEqual(expect.any(Function))
+    expect(createFlpPipeline).toEqual(expect.any(Function))
+    expect(createTransmittable).toEqual(expect.any(Function))
+    expect(createMaskerPipe).toEqual(expect.any(Function))
   })
 })
