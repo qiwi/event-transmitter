7f6c93339756ec7ccdbe9bbf7eae8b996c362059
Anton Golub <golub.anton@gmail.com>
2021-02-25 16:30:15 +0300

    fix(package): up deps, fix vuls, update ci flow (#30)

---
+++ src/main/ts/utils/identity.ts
@@ -0,0 +1,1 @@
+export const identity = <T>(v:T):T => v
--- src/test/ts/pipes/http.ts
+++ src/test/ts/pipes/http.ts
@@ -25,7 +25,7 @@ describe('httpPipe', () => {
           email: 'janet.weaver@reqres.in',
           first_name: 'Janet',
           last_name: 'Weaver',
-          avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg',
+          avatar: expect.any(String),
         },
       }])
   })
