86ba3fc871bfe4b07faa8a019af470a9786668a9
Anton Golub <mailbox@antongolub.ru>
2021-03-04 19:03:27 +0300

    feat: handle armv7l ua

--- src/test/ts/pipes/deviceInfo.ts
+++ src/test/ts/pipes/deviceInfo.ts
@@ -13,7 +13,8 @@ describe('deviceInfoPipe', () => {
 
 describe('getDeviceInfo', () => {
   it('returns device info', () => {
-    const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    const userAgent =
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     const expected: IDeviceInfo = {
       browser: {
         name: 'Chrome',
