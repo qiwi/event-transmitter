6d9fa14a8418deeab322fa91ebdb3b87032e46b7
Anton Golub <golub.anton@gmail.com>
2021-03-08 20:24:33 +0300

    fix(pkg): up deps, fix vuls (#32)

--- src/main/ts/utils/deviceTypes.ts
+++ src/main/ts/utils/deviceTypes.ts
@@ -1,7 +1,3 @@
 import { isMobile as _isMobile } from 'is-mobile'
 
-// + handle 'armv7l'
-// Mozilla/5.0 (X11; ; U; Linux armv7l; en-gb) AppleWebKit/534.26+ (KHTML, like Gecko) Version/5.0 Safari/534.26+
-
-export const isMobile = (userAgent: string): boolean =>
-  /armv7l/i.test(userAgent) || _isMobile({ ua: userAgent, tablet: true })
+export const isMobile = (ua: string): ReturnType<typeof _isMobile> => _isMobile({ ua, tablet: true })
