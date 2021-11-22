849207f3c81e2bf10b01a658280367d736e8e607
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2021-11-22 17:12:12 +0300

    feat: send userAgent instead of deviceInfo

--- src/main/ts/pipes/deviceInfo.ts
+++ src/main/ts/pipes/deviceInfo.ts
@@ -1,33 +1,14 @@
-import { IDeviceInfo, IPromise } from '@qiwi/substrate'
-import platform from 'platform'
+import { IPromise } from '@qiwi/substrate'
 
 import { IPipe, IPipeOutput, ITransmittable } from '../interfaces'
-import { clone, isMobile, set } from '../utils'
+import { clone, set } from '../utils'
 
 export const type = 'device-info'
-
-export const getDeviceInfo = (userAgent?: string): IDeviceInfo => {
-  const parsedData = userAgent ? platform.parse(userAgent) : platform
-  const { name, version, layout, product, manufacturer, os } = parsedData
-  return {
-    browser: {
-      name,
-      version,
-      layout,
-    },
-    isMobile: isMobile(userAgent || window.navigator.userAgent),
-    model: {
-      name: product,
-      manufacturer,
-    },
-    os,
-  }
-}
 
 export const createDeviceInfoPipe = (): IPipe => ({
   type,
   execute({ data }: ITransmittable): IPromise<IPipeOutput> {
-    const output = set(clone(data), 'meta.deviceInfo', getDeviceInfo())
+    const output = set(clone(data), 'meta.userAgent', window.navigator.userAgent)
     return Promise.resolve([null, output])
   },
 })
--- src/main/ts/utils/deviceTypes.ts
+++
@@ -1,3 +0,0 @@
-import { isMobile as _isMobile } from 'is-mobile'
-
-export const isMobile = (ua: string): ReturnType<typeof _isMobile> => _isMobile({ ua, tablet: true })
