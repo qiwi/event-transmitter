bed0b1637aff7fa8d14de0ad1de3d5ad98238778
pismenskiy <pismenskiy.m@gmail.com>
2022-07-04 18:05:18 +0300

    feat: move to github actions

--- src/test/js/index.js
+++ src/test/js/index.js
@@ -1,7 +1,7 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 
-import { createHttpPipe,createTransmitter } from '../../../target/es5/index.js'
+import { createHttpPipe,createTransmitter } from '../../../target/es5/index'
 
 test('index.js (target)', () => {
   test('has proper export', () => {
