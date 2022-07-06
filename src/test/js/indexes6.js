a98ce7830f92c7f6eacc899f4dc1a6e336d92352
Maxim Pismenskiy <pismenskiy.m@gmail.com>
2022-07-06 16:38:24 +0300

    fix: move substrate to deps

---
+++ src/test/js/indexes6.js
@@ -0,0 +1,14 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import { createFrontLogProxyTransmitter,createHttpPipe,createTransmitter } from '../../../target/es6/index.mjs'
+
+test('index es6 (target)', () => {
+  test('has proper export', () => {
+    assert.instance(createTransmitter, Function)
+    assert.instance(createHttpPipe, Function)
+    assert.instance(createFrontLogProxyTransmitter, Function)
+  })
+})
+
+test.run()
